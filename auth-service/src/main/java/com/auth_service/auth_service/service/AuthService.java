fac5def214f74a1342b8d2f3b4ed540b4aba579c
Nathaly <nathaly.jp03@gmail.com>
2025-08-14 16:25:10 -0300

    Ajustes finais

--- auth-service/src/main/java/com/auth_service/auth_service/service/AuthService.java
+++ auth-service/src/main/java/com/auth_service/auth_service/service/AuthService.java
@@ -6,24 +6,29 @@ import com.auth_service.auth_service.dto.ResponseDto;
 import com.auth_service.auth_service.enuns.Role;
 import com.auth_service.auth_service.model.User;
 import com.auth_service.auth_service.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
     //persistindo dados
-    private UserRepository userRepository;
-    private JwtService jwtService;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthService (UserRepository userRepository, JwtService jwtService){
+    public AuthService (UserRepository userRepository, JwtService jwtService, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
         this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
     }
+
 
     public String userLogin (AuthDto authDto){
         User user = userRepository.findByEmail(authDto.getEmail()).orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
 
-        if(!user.getPassword().equals(authDto.getPassword())){
+        if(!passwordEncoder.matches(authDto.getPassword(), user.getPassword())){
             throw new RuntimeException("Senha incorreta!");
         }
 
@@ -39,7 +44,7 @@ public class AuthService {
             throw new RuntimeException("Email já cadastrado!");
         }
 
-        User user = new User(null, registerDto.getNome(), registerDto.getEmail(), registerDto.getPassword(), registerDto.getRole());
+        User user = new User(null, registerDto.getNome(), registerDto.getEmail(), passwordEncoder.encode(registerDto.getPassword()), registerDto.getRole());
         userRepository.save(user);
 
         return new ResponseDto(user.getId(), user.getNome(), user.getEmail(), user.getRole());
